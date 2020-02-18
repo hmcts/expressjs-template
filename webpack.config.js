81ed6e44e312a8879cd7b3d58886f836fba0ca0e
Tim Jacomb <tim.jacomb@hmcts.net>
2020-02-18 12:18:12 +0000

    Working webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,51 @@
 const path = require('path');
-var nodeExternals = require('webpack-node-externals');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const govukFrontEndPath = path.join(__dirname, 'node_modules/govuk-frontend/govuk')
-const govukImagePath = path.resolve(govukFrontEndPath, 'assets/images')
-const govukFontsPath = path.resolve(govukFrontEndPath, 'assets/fonts')
-const govukscssPath = path.resolve(govukFrontEndPath, 'all.scss')
-
-const miniCssExtractPlugin = new MiniCssExtractPlugin({
-    // Options similar to the same options in webpackOptions.output
-    // both options are optional
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-})
-const copyGovukTemplateAssets = new CopyWebpackPlugin([
-    { from: govukImagePath, to: 'img' },
-    { from: govukFontsPath, to: 'fonts' }
-]);
-module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss']
-    },
-    //target: 'node',
-    externals: [nodeExternals()],
-    entry: path.join(__dirname, 'src/main/modules/govukfrontend/', 'index.ts'),
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        sourceMapFilename: '[name].js.map'
-    },
-    plugins: [
-        copyGovukTemplateAssets,
-        miniCssExtractPlugin
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [// fallback to style-loader in development
-                    process.env.NODE_ENV !== 'production'
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            includePaths: [govukscssPath],
-                            sassOptions: {
-                                indentWidth: 4,
-                                includePaths: [govukscssPath],
-                            },
-                        }
-                    }],
-                include: [
-                    govukscssPath,
-                ]
-            }
-        ]
-    },
-
+const config = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          warnings: false,
+          compress: {
+            ie8: true,
+          },
+          mangle: {
+            ie8: true
+          },
+          output: {
+            comments: false,
+            ie8: true
+          }
+        }
+      })
+    ]
+  },
+  entry: './src/main/assets/main.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map'
+  }
 };
+
+module.exports = config;
