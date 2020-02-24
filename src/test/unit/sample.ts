01a23c6c6458bb69f1066fa405f45408fd953071
Tim Jacomb <tim.jacomb@hmcts.net>
2020-02-24 14:30:43 +0000

    Update src/test/unit/sample.ts

--- src/test/unit/sample.ts
+++ src/test/unit/sample.ts
@@ -1,4 +1,4 @@
-describe("Example test to satisfy mocha (to be removed from your app)", () => {
+describe("Example test to satisfy jest (to be removed from your app)", () => {
    test("to be removed from your app", async () => {
     });
 });
