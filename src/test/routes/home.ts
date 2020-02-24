9f90d6d748a506a26bb70a3a3b8f01d367d41c09
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-02-24 14:32:16 +0000

    Moving to Jest (#30)

--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -6,7 +6,7 @@ import { app } from "../../main/app";
 // TODO: replace this sample test with proper route tests for your application
 describe("Home page", () => {
   describe("on GET", () => {
-    it("should return sample home page", async () => {
+    test("should return sample home page", async () => {
       await request(app)
         .get("/")
         .expect((res) => expect(res.status).to.equal(200));
