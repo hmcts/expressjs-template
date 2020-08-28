2ac2eb6cc302ebb0cf96566fd50a08fe697e3abe
Linus Norton <linusnorton@gmail.com>
2020-08-28 16:44:10 +0100

    update routeing so it doesn't rely on module exports

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -1,15 +1,14 @@
+import { Application } from 'express';
+
 const healthcheck = require('@hmcts/nodejs-healthcheck');
-import * as express from 'express';
 
-const router = express.Router();
+export default function(app: Application) {
+  const healthCheckConfig = {
+    checks: {
+      // TODO: replace this sample check with proper checks for your application
+      sampleCheck: healthcheck.raw(() => healthcheck.up()),
+    },
+  };
 
-const healthCheckConfig = {
-  checks: {
-    // TODO: replace this sample check with proper checks for your application
-    sampleCheck: healthcheck.raw(() => healthcheck.up()),
-  },
-};
-
-healthcheck.addTo(router, healthCheckConfig);
-
-module.exports = router;
+  healthcheck.addTo(app, healthCheckConfig);
+}
