b98f3533870fe767e7700987f106c943817cab35
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-09-11 12:45:50 +0100

    Sync changes from template-expressjs (#62)

--- webpack/app.js
+++ webpack/app.js
@@ -5,9 +5,9 @@ const root = path.resolve(__dirname, './../../');
 const sass = path.resolve(root, './main/assets/scss');
 const images = path.resolve(root, './main/assets/images');
 
-const copyLookAndFeelAssets = new CopyWebpackPlugin(
-  [{ from: images, to: 'images' }],
-);
+const copyLookAndFeelAssets = new CopyWebpackPlugin({
+  patterns: [{ from: images, to: 'images' }],
+});
 
 module.exports = {
   paths: { root, sass },
