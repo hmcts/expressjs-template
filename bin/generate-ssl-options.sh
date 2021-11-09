6fbace1e1c2d6275ffbe84f475c5650d895fb086
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2021-11-09 17:52:13 +0000

    Fix typo

--- bin/generate-ssl-options.sh
+++ bin/generate-ssl-options.sh
@@ -4,17 +4,17 @@
 # These resources are used for exposing the application via HTTPS in development environment. They must not be used
 # in other environments, as their purpose is to provide consistency, not security.
 
-localhost_ssh_folder="src/main/resources/localhost-ssl"
+localhost_ssl_folder="src/main/resources/localhost-ssl"
 
-if [ ! -f "$localhost_ssh_folder"/localhost.key -o ! -f "$localhost_ssh_folder"/localhost.crt ]
+if [ ! -f "$localhost_ssl_folder"/localhost.key -o ! -f "$localhost_ssl_folder"/localhost.crt ]
 then
-  mkdir -p "$localhost_ssh_folder"
+  mkdir -p "$localhost_ssl_folder"
   openssl req \
     -nodes \
     -x509 \
     -newkey rsa:4096 \
-    -keyout "$localhost_ssh_folder"/localhost.key \
-    -out "$localhost_ssh_folder"/localhost.crt \
+    -keyout "$localhost_ssl_folder"/localhost.key \
+    -out "$localhost_ssl_folder"/localhost.crt \
     -sha256 \
     -days 3650 \
     -subj "/C=GB/ST=A/L=B/O=C/OU=D/CN=E" \
