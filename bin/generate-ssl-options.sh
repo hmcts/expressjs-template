f7265058de8c156ade6a5f90819dfa7f17b23147
Tim Jacomb <tim.jacomb@hmcts.net>
2021-11-10 11:43:55 +0000

    Cleanup

--- bin/generate-ssl-options.sh
+++ bin/generate-ssl-options.sh
@@ -7,16 +7,6 @@
 
 localhost_ssl_folder="src/main/resources/localhost-ssl"
 
-openssl_command="openssl req \
-               -nodes \
-               -x509 \
-               -newkey rsa:4096 \
-               -keyout "$localhost_ssl_folder"/localhost.key \
-               -out "$localhost_ssl_folder"/localhost.crt \
-               -sha256 \
-               -days 3650 \
-               -subj "/C=GB/ST=A/L=B/O=C/OU=D/CN=E""
-
 if [ ! -f "$localhost_ssl_folder"/localhost.key -o ! -f "$localhost_ssl_folder"/localhost.crt ]
 then
   mkdir -p "$localhost_ssl_folder"
