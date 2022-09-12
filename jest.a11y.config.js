d63a31fd699146c099f22b9e251c8cb8afaeb78b
Tim Jacomb <tim.jacomb@hmcts.net>
2022-09-12 12:28:50 +0100

    Remove duplicate module file extension

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src/test/a11y'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
