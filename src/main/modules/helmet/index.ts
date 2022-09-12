1d3772aec28b2864c26b922cae6930343eaf8bfb
Tim Jacomb <tim.jacomb@hmcts.net>
2022-09-12 16:08:46 +0100

    Fix CSP error related to webpack

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -8,8 +8,23 @@ const self = "'self'";
  * Module that enables helmet in the application
  */
 export class Helmet {
+  private readonly developmentMode: boolean;
+  constructor(developmentMode: boolean) {
+    this.developmentMode = developmentMode;
+  }
+
   public enableFor(app: express.Express): void {
     // include default helmet functions
+    const scriptSrc = [self, googleAnalyticsDomain, "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"];
+
+    if (this.developmentMode) {
+      // Uncaught EvalError: Refused to evaluate a string as JavaScript because 'unsafe-eval'
+      // is not an allowed source of script in the following Content Security Policy directive:
+      // "script-src 'self' *.google-analytics.com 'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='".
+      // seems to related to webpack
+      scriptSrc.push("'unsafe-eval'");
+    }
+
     app.use(
       helmet({
         contentSecurityPolicy: {
@@ -19,7 +34,7 @@ export class Helmet {
             fontSrc: [self, 'data:'],
             imgSrc: [self, googleAnalyticsDomain],
             objectSrc: [self],
-            scriptSrc: [self, googleAnalyticsDomain, "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+            scriptSrc,
             styleSrc: [self],
           },
         },
