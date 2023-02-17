a80723eae43fd270e1cd15715968f093f1f872e9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-17 15:14:55 +0000

    update workflow

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src/test/a11y'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  testTimeout: 20000,
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
