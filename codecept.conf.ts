e6cdd6416fb3324e2b0cba2cf71a4ed7ef47702c
Daniel Furnivall <danielfurnivall@gmail.com>
2023-08-30 23:12:09 +0100

    added forgotten bits

---
+++ codecept.conf.ts
@@ -0,0 +1,31 @@
+console.log('Debug: Entering codecept.conf.ts');
+
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+setHeadlessWhen(testConfig.TestHeadlessBrowser);
+export const config: CodeceptJS.MainConfig = {
+  name: 'functional',
+  gherkin: testConfig.Gherkin,
+  output: './functional-output/functional/reports',
+  helpers: testConfig.helpers,
+  tests: './*_test.{js,ts}',
+  plugins: {
+    allure: {
+      enabled: true,
+      require: '@codeceptjs/allure-legacy'
+    },
+    pauseOnFail: {
+      enabled: !testConfig.TestHeadlessBrowser,
+    },
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+      fullPageScreenshots: true,
+    },
+  },
+};
