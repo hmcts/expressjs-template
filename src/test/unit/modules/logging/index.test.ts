7f09b9626f64b5b95bdf0073f1567de97ae9035b
Michael1142 <michael1142@hotmail.co.uk>
2026-07-27 17:52:38 +0100

    Add logging test

---
+++ src/test/unit/modules/logging/index.test.ts
@@ -0,0 +1,10 @@
+import { getLogger } from '../../../../main/modules/logging';
+
+describe('getLogger', () => {
+  it('returns a logger for the requested component', () => {
+    const logger = getLogger('test-component');
+
+    expect(logger).toBeDefined();
+    expect(logger.info).toEqual(expect.any(Function));
+  });
+});
